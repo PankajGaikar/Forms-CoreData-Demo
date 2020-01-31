96d5dd6dd2b372ea6bb35d0025046c30f65891bf
PANKYAAA <pankajs.gaikar@gmail.com>
2020-01-31 23:29:16 +0530

    Added project structure. - View Controllers - TableViewCell - ViewModels - Model class - StorageService - To Interact with Core Data.

---
+++ Forms-CoreData-Demo/Utility/StorageService.swift
@@ -0,0 +1,13 @@
+//
+//  StorageService.swift
+//  Forms-CoreData-Demo
+//
+//  Created by Pankaj Gaikar on 31/01/20.
+//  Copyright © 2020 Pankaj Gaikar. All rights reserved.
+//
+
+import Foundation
+
+class StorageService {
+    
+}
---
+++ Forms-CoreData-Demo/View/TableViewCell/FormDataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FormDataTableViewCell.swift
+//  Forms-CoreData-Demo
+//
+//  Created by Pankaj Gaikar on 31/01/20.
+//  Copyright © 2020 Pankaj Gaikar. All rights reserved.
+//
+
+import UIKit
+
+class FormDataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
